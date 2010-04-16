0425b705ec78621bfe71cad9be8d21fa8a3880d4
Marc Liyanage <liyanage@primavera.local>
2010-04-15 23:23:28 -0700

    Adding ruby version of git-svn-clone-externals script, work in progress.

---
+++ git-svn-clone-externals.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/ruby
+
+class ExternalsProcessor
+
+
+def initialize(options = {})
+	@parent = options['parent'];
+	ENV['PATH'] = "/opt/local/bin:#{ENV['PATH']}";
+	@svn_info = svn_info_for_current_dir()
+end
+
+
+def svn_info_for_current_dir
+	lines = shell("git svn info")
+	svn_info = {};
+	pairs = lines.map {|x| x.split(": ")}
+	pairs.each {|k, v| svn_info[k] = v}
+	return svn_info
+end
+
+
+def known_url?(url)
+	return url == @svn_info['URL'] || (@parent && @parent.known_url?(url))
+end
+
+
+def shell(cmd)
+	list = %x(#{cmd}).split("\n")
+	status = $? >> 8
+	raise "Non-zero exit status #{status} for command #{cmd}" if status != 0
+	return list
+end
+
+
+def run
+
+	externals = read_externals()
+	p externals
+
+return 0
+
+end
+
+
+def read_externals
+	externals = shell('git svn show-externals').reject {|x| x =~ %r%^\s*/?\s*#%}
+	versioned_externals = externals.grep(/-r\d+\b/i)
+	if !versioned_externals.empty?
+		raise "Error: Found external(s) pegged to fixed revision: '#{versioned_externals.join ', '}' in '#{Dir.getwd}', don't know how to handle this.\n"
+	end
+
+	p externals
+
+#	todo: split into dir/url hash
+	return externals
+end
+
+#	foo = ['/#foo', 'bar', 'baz -r52', 'baz2 -r53']
+#	externals = foo.reject {|x| x =~ %r%^\s*/?\s*#%}
+
+
+end
+
+
+processor = ExternalsProcessor.new
+status = processor.run()
+exit status
+
