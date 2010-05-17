0281ca5b43f592881304387c1fd46790fcfef1af
Marc Liyanage <liyanage@liyanage1.apple.com>
2010-05-17 09:39:23 -0700

    Adding Xcode "git annotate" user script

---
+++ xcode-git-annotate.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby -w
+
+selection_start = %%%{PBXSelectionStart}%%%
+line_start = 0
+selection_line_number = 1
+STDIN.each do |line|
+	line_end = line_start + line.length
+	line_range = line_start ... line_end
+	break if line_range.include? selection_start
+	line_start += line.length
+	selection_line_number += 1
+end
+
+dir = File.dirname '%%%{PBXFilePath}%%%'
+file = ENV['FILENAME']
+Dir.chdir dir
+
+output_line_number = 1
+%x(git annotate '#{file}').each do |line|
+	if output_line_number == selection_line_number
+		print '%%%{PBXSelection}%%%'
+		print line
+		print '%%%{PBXSelection}%%%'
+	else
+		print line
+	end
+	output_line_number += 1
+end
