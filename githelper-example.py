68839ebb43126d1267977d831a4a885b91392169
Marc Liyanage <reg.gitcommit@entropy.ch>
2012-09-19 11:26:36 -0700

    Add githelper-example.py

---
+++ githelper-example.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import githelper
+import os
+import sys
+
+Class Foo:
+
+	def __init__(self, some_state):
+		self.some_state = some_state
+
+	def __call__(self, wc):
+		# possibly use self.some_state
+		print wc.current_branch()	
+
+def process(wc):
+	print wc.current_branch()
+
+# Construct a GitWorkingCopy instance with a path
+root_wc = githelper.GitWorkingCopy(sys.argv[1])
+
+# Now process this working copy and all sub working copies recursively.
+
+# Variant 1: Iterate over self_and_descendants()
+for wc in root_wc.self_and_descendants():
+	print wc.current_branch()
+
+# Variant 2: Pass a function to traverse(), the function gets called for and with each working copy
+root_wc.traverse(process)
+
+# Variant 3: Pass an object that implements __call__, the method gets called for and with each working copy
+iterator = Foo('bar')
+root_wc.traverse(iterator)
+
+
